9188ed094980bd15634c0674ac3ab204bab13b33
Sladkiy Eduard <nikcxprojects@gmail.com>
2023-06-16 13:53:02 +0300

    update

--- Assets/appwinline/Scripts/Managers/UIManager.cs
+++ Assets/appwinline/Scripts/Managers/UIManager.cs
@@ -16,32 +16,18 @@ public class UIManager : MonoBehaviour
     [SerializeField] GameObject settings;
     [SerializeField] GameObject top;
     [SerializeField] GameObject game;
-    [SerializeField] GameObject pause;
-    [SerializeField] GameObject result;
 
     [Space(10)]
     [SerializeField] Text scoreText;
-    [SerializeField] Text finalScoreText;
 
     private void Awake()
     {
-        Box.OnCollisionAction += (ball, boxDirection) =>
+        Cup.OnEndShaking += (dice1Value, dice2Value, dice3Value) =>
         {
-            var trueDirection = ball.Direction == boxDirection;
-            if(trueDirection)
-            {
-                score++;
-
-                scoreText.text = $"{score}";
-                finalScoreText.text = $"<size=50><color=#17263D>SCORE</color></size>\r\n<color=#D42A28>{score}</color>";
-            }
-            else
-            {
-                GameOver();
-            }
+            score += (dice1Value + dice2Value + dice3Value);
+            scoreText.text = $"{score}";
         };
     }
-
 
     private void Start()
     {
@@ -50,12 +36,8 @@ public class UIManager : MonoBehaviour
 
     public void StartGame()
     {
-        Time.timeScale = 1;
-
         score = 0;
-
         scoreText.text = $"{score}";
-        finalScoreText.text = $"<size=50><color=#17263D>SCORE</color></size>\r\n<color=#D42A28>{score}</color>";
 
         var _parent = GameObject.Find("Environment").transform;
         var _prefab = Resources.Load<GameObject>("level");
@@ -63,8 +45,6 @@ public class UIManager : MonoBehaviour
         _gameRef = Instantiate(_prefab, _parent);
 
         menu.SetActive(false);
-        result.SetActive(false);
-        
         game.SetActive(true);
     }
 
@@ -80,12 +60,6 @@ public class UIManager : MonoBehaviour
         menu.SetActive(false);
     }
 
-    public void SetPause(bool IsPause)
-    {
-        Time.timeScale = IsPause ? 0 : 1;
-        pause.SetActive(IsPause);
-    }
-
     public void OpenMenu()
     {
         if (_gameRef)
@@ -96,20 +70,7 @@ public class UIManager : MonoBehaviour
         game.SetActive(false);
         settings.SetActive(false);
         top.SetActive(false);
-        pause.SetActive(false);
-        result.SetActive(false);
 
         menu.SetActive(true);
-    }
-
-    public void GameOver()
-    {
-        if (_gameRef)
-        {
-            Destroy(_gameRef);
-        }
-
-        game.SetActive(false);
-        result.SetActive(true);
     }
 }
